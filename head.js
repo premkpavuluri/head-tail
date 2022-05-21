f1b5209ff37e4b2a8de934d8c1a1e146b4ec6c2a
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-05-21 11:43:39 +0530

    Connected `head` to command line.

--- head.js
+++ head.js
@@ -1,1 +1,4 @@
-console.log('usage: head [-n lines | -c bytes] [file ...]');
+const fs = require('fs');
+const { headMain } = require('./src/headLib.js');
+
+console.log(headMain(fs.readFileSync, ...process.argv.slice(2)));
